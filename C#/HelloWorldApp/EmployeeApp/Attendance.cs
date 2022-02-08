0c49547ab043d6728c0751feeb90f54ea5f84a36
Nishanth <rajkumarnishanth@gmail.com>
2022-02-08 22:38:10 +0530

    Uploaded EmployeeApp and HElloworld C# projects

---
+++ C#/HelloWorldApp/EmployeeApp/Attendance.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeApp
+{
+    internal class Attendance
+    {
+        public static void GetAttendance()
+        {
+            int IS_FULL_TIME = 1;
+            Random random = new Random();
+            int empCheck = random.Next(0, 2);
+            if (empCheck == IS_FULL_TIME)
+                Console.WriteLine("Employee is present");
+            else
+                Console.WriteLine("Employee is absent");
+        }
+
+    }
+}
---
+++ C#/HelloWorldApp/EmployeeApp/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace EmployeeApp
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            //Attendance.GetAttendance();
+            //DailyWage.EmpWage();
+            //PartTime.PartTimeEmpWage();
+            //WageUsingCase.EmpWageUsingSwitch();
+            CalculateWageForMonth.CalculateWagesFor20DaysInMonth();
+        }
+    }
+}

be33741d21be2bdeaeb6c652571bb3241212d3c4
Nishanth <rajkumarnishanth@gmail.com>
2022-02-13 20:05:23 +0530

    Tested readonly concept. Observed how it differed from constant

--- ProjectHub/ConceptTesting/Program.cs
+++ ProjectHub/ConceptTesting/Program.cs
@@ -1,8 +1,15 @@
-﻿using ConceptTesting.StaticTests;
+﻿using ConceptTesting.ReadOnlyTests;
+using ConceptTesting.StaticTests;
 
 Console.Title = "Concept Testing App";
 
 //StaticTest.ConstructorTesting();
-StaticTest.TestStaticClass();
+//StaticTest.TestStaticClass();
+
+// ReadOnly Testing
+ReadOnlyTest test1 = new();
+ReadOnlyTest test2 = new(5);
+test1.Display();
+test2.Display();
 
 Console.ReadKey();
---
+++ ProjectHub/ConceptTesting/ReadOnlyTests/ReadOnlyTest.cs
@@ -0,0 +1,26 @@
+﻿namespace ConceptTesting.ReadOnlyTests;
+
+
+// This class is used test the readonly and observe it's difference from constant
+internal class ReadOnlyTest
+{
+    // Decalaring and intializing the readonly variable with a value
+    private readonly int num = 2;
+
+    // Testing if readonly value can be changed in constructor
+    public ReadOnlyTest()
+    {
+        num++;
+    }
+
+    // parametrised constructor
+    public ReadOnlyTest(int a)
+    {
+        num = a;
+    }
+
+    public void Display()
+    {
+        Console.WriteLine("num: " + num);
+    }
+}

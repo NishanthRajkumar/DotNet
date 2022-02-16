76902b838d64c6d72c36de083ce6b4b809e53b55
Nishanth <rajkumarnishanth@gmail.com>
2022-02-16 10:06:52 +0530

    Testing of the GetClassLogger of Logmanager

--- ClassRoom/NLogDemo/AddNumbers.cs
+++ ClassRoom/NLogDemo/AddNumbers.cs
@@ -10,12 +10,14 @@ namespace NLogDemo;
 internal class AddNumbers
 {
     NLog nLog = new NLog();
+    NLog2 nLog2 = new NLog2();
     public int Sum(int a, int b)
     {
         if (a == 0 || b == 0)
         {
             nLog.LogDebug("Debug Successfull : Sum()");
             nLog.LogError("Expecting null values");
+            nLog2.LogError("Expecting null values. Sent from NLog2 Object");
             nLog.LogWarn("a or b should not equal to 0");
         }
         int c = a + b;

1056efa71f8cf4b70aa1809bfbe46e9c705ea0ca
Nishanth <rajkumarnishanth@gmail.com>
2022-02-24 11:20:29 +0530

    Added Hashtable concept project

---
+++ ClassRoom/HashTableDemo/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace HashTableDemo;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        //Hashtable ht = new Hashtable();
+        Console.WriteLine("Hash table demo"); //() []
+        MyMapNode<string, string> hash = new MyMapNode<string, string>(5);
+        hash.Add("0", "To");
+        hash.Add("1", "be");
+        hash.Add("2", "or");
+        hash.Add("3", "not");
+        hash.Add("4", "to");
+        hash.Add("5", "Not be");
+        hash.Add("5", "be");
+        hash.Add("5", "be 2");
+
+        string hash5 = hash.Get("5");
+        Console.WriteLine("5th index value: " + hash5);
+        string hash2 = hash.Get("2");
+        Console.WriteLine("2th index value: " + hash2);
+        Console.ReadKey();
+
+
+        ///////////////
+        ///
+        /*ht.Add(14, 135.789);
+        ht.Add("key", 130);
+        ht.Add(14.0, "Value");
+        ht.Add(14, 135.789);*/
+
+    }
+}

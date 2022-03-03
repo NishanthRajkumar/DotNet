936bc379999dc82bd75d00cd3e5bfd03b5089c51
Nishanth <rajkumarnishanth@gmail.com>
2022-03-03 22:28:01 +0530

    Testing Exception handling concept using threads

---
+++ ProjectHub/ExceptionHandling/Program.cs
@@ -0,0 +1,19 @@
+﻿using ExceptionHandling;
+
+Console.Title = "Exception Handling testing App";
+Console.WriteLine("==========Exception Handling==========");
+
+Thread t1 = new Thread(() => Test.ConvertStringToInt(""));
+Thread t2 = new Thread(() => Test.TryConvertStringToInt(""));
+
+t1.Start();
+//t2.Start();
+
+while (t1.IsAlive) ;
+//while (t2.IsAlive) ;
+
+//Test.TryConvertStringToInt("sdfsf");
+
+Console.WriteLine("Thread completed and continuing in Main");
+
+Console.ReadKey();
---
+++ ProjectHub/ExceptionHandling/Test.cs
@@ -0,0 +1,23 @@
+﻿namespace ExceptionHandling;
+
+internal static class Test
+{
+    public static void ConvertStringToInt(string txt)
+    {
+        Thread.Sleep(3000);
+        int num = Convert.ToInt32(txt);
+    }
+
+    public static void TryConvertStringToInt(string txt)
+    {
+        Thread.Sleep(3000);
+        try
+        {
+            int num = Convert.ToInt32(txt);
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+    }
+}

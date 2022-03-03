0ba56bb6750d3f11c5288a5218855c6f00286c52
Nishanth <rajkumarnishanth@gmail.com>
2022-03-03 07:33:33 +0530

    Added CoffeeMaker project
    Added a CoffeeTest class in UnitTest
    to test the CoffeeMaker project

--- ClassRoom/CoffeeMaker/Program.cs
+++ ClassRoom/CoffeeMaker/Program.cs
@@ -1,2 +1,2 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Title = "Coffe maker test App";
+Console.WriteLine("==========Coffee maker==========");
--- ClassRoom/UnitTest/CoffeeTest.cs
+++ ClassRoom/UnitTest/CoffeeTest.cs
@@ -2,41 +2,39 @@ using CoffeeMaker;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Moq;
 
-namespace CoffeeMakerTest
+namespace CoffeeMakerTest;
+
+[TestClass]
+public class CoffeeTest
 {
-    [TestClass]
-    public class CoffeeTest
+    [TestMethod]
+    public void OrderACoffee_Should_return_Received_Message()
     {
-        [TestMethod]
-        public void OrderACoffee_Should_return_Received_Message()
-        {
-            StarbuckStore store = new(new Starbucks());
-            string result = store.OrderCoffee(2, 4);
-            Assert.AreEqual("Your Order is received.", result);
-        }
+        StarbuckStore store = new(new Starbucks());
+        string result = store.OrderCoffee(2, 4);
+        Assert.AreEqual("Your Order is received.", result);
+    }
 
-        [TestMethod]
-        public void OrderACoffee_Should_Return_Received_MessageUsingStub()
-        {
-            StarbuckStore store = new(new StubStarbucks());
-            string result = store.OrderCoffee(2, 4);
-            Assert.AreEqual("Your order is received.", result);
-        }
+    [TestMethod]
+    public void OrderACoffee_Should_Return_Received_MessageUsingStub()
+    {
+        StarbuckStore store = new(new StubStarbucks());
+        string result = store.OrderCoffee(2, 4);
+        Assert.AreEqual("Your order is received.", result);
+    }
 
-        //Moq MOCK
+    //Moq MOCK
+    [TestMethod]
+    public void OrderACoffee_Should_Return_Received_MessageUsingMock()
+    {
+        var service = new Mock<StubStarbucks>();
+        service.As<IMakeACoffee>().Setup(x => x.CheckIngredientAvailability()).Returns(true);
+        service.As<IMakeACoffee>().Setup(x => x.CoffeeMaking(It.IsAny<int>(), It.IsAny<int>())).Returns("Your Order is received1.");
+        //service.As<IMakeACoffee>().Setup(x => x.CheckIngredientAvailability()).Returns(false);
 
-        [TestMethod]
-        public void OrderACoffee_Should_Return_Received_MessageUsingMock()
-        {
-            var service = new Mock<StubStarbucks>();
-            service.As<IMakeACoffee>().Setup(x => x.CheckIngredientAvailability()).Returns(true);
-            service.As<IMakeACoffee>().Setup(x => x.CoffeeMaking(It.IsAny<int>(), It.IsAny<int>())).Returns("Your Order is received1.");
-            //service.As<IMakeACoffee>().Setup(x => x.CheckIngredientAvailability()).Returns(false);
+        StarbuckStore store = new(service.Object);
+        var result = store.OrderCoffee(2, 4);
+        Assert.AreEqual("Your Order is received.", result);
 
-            StarbuckStore store = new(service.Object);
-            var result = store.OrderCoffee(2, 4);
-            Assert.AreEqual("Your Order is received.", result);
-
-        }
     }
 }

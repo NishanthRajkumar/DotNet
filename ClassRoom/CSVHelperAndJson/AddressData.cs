68c283c427462689cf064cfafbb1d7f2ffcad282
Nishanth <rajkumarnishanth@gmail.com>
2022-03-11 10:03:05 +0530

    Added new project 'CSVHelperAndJson'

---
+++ ClassRoom/CSVHelperAndJson/AddressData.cs
@@ -0,0 +1,11 @@
+﻿namespace CSVHelperAndJson;
+
+internal class AddressData
+{
+    public string firstname { get; set; }
+    public string lastname { get; set; }
+    public string address { get; set; }
+    public string city { get; set; }
+    public string state { get; set; }
+    public string code { get; set; }
+}

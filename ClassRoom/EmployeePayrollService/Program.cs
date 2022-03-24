f99d1a8bbfbb817b1683daaa5d0f287fddb6d19d
Nishanth <rajkumarnishanth@gmail.com>
2022-03-24 11:31:58 +0530

    [Nish] Added new porjects and updated old ones
    EmployeePayrollService and EmployeePayrollTest has been added

---
+++ ClassRoom/EmployeePayrollService/Program.cs
@@ -0,0 +1,18 @@
+﻿using EmployeePayrollService;
+
+Console.WriteLine("Welcome to Employee Payroll!");
+EmployeeRepo repo = new();
+EmployeeModel employee = new();
+employee.EmployeeName = "Rinki";
+employee.Department = "Mechanical";
+employee.PhoneNumber = "7474362528";
+employee.Address = "Delhi";
+employee.Gender = 'F';
+employee.BasicPay = 12000.00M;
+employee.Deductions = 1500.00;
+employee.StartDate = Convert.ToDateTime("2020-11-03");
+
+if (repo.AddEmployee(employee))
+    Console.WriteLine("Records added successfully");
+//repo.GetAllEmployee();
+Console.ReadKey();
